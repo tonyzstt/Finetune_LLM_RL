e9565fd99dc046a070011d6fc4d08914412141de
Feiyang Zhu <feiyangzhu12@gmail.com>
2025-05-20 18:05:30 +0000

    update RLOO to online

--- RLOO/rloo_dataset.py
+++ RLOO/rloo_dataset.py
@@ -5,7 +5,7 @@ class RLOODataset(Dataset):
     def __init__(self, data, tokenizer, task, max_length=1024):
         self.data = data
         self.tokenizer = tokenizer
-        # task: string, either "ultrafeedback" or "countdown"
+        # task: str, either "ultrafeedback" or "countdown"
         self.task = task
         self.max_length = max_length
 
@@ -14,40 +14,18 @@ class RLOODataset(Dataset):
 
     def __getitem__(self, idx):
         example = self.data[idx]
-        prompt = example['input']
-        chosen = example['chosen']
-        rejected = example['rejected']
-
-        prompt_chosen = prompt + chosen
-        prompt_rejected = prompt + rejected
-
-        enc_chosen = self.tokenizer(prompt_chosen, padding='max_length', truncation=True, max_length=self.max_length, return_tensors='pt')
-        enc_rejected = self.tokenizer(prompt_rejected, padding='max_length', truncation=True, max_length=self.max_length, return_tensors='pt')
-        enc_prompt = self.tokenizer(prompt, padding='max_length', truncation=True, max_length=self.max_length, return_tensors='pt')
+        prompt = example["input"]
+        enc_prompt = self.tokenizer(prompt, padding="max_length", truncation=True, max_length=self.max_length, return_tensors="pt")
 
         if self.task == "ultrafeedback":
-
             return {
-                'input_ids_prompt': enc_prompt['input_ids'].squeeze(0),
-                'attention_mask_prompt': enc_prompt['attention_mask'].squeeze(0),
-                'input_ids_chosen': enc_chosen['input_ids'].squeeze(0),
-                'attention_mask_chosen': enc_chosen['attention_mask'].squeeze(0),
-                'input_ids_rejected': enc_rejected['input_ids'].squeeze(0),
-                'attention_mask_rejected': enc_rejected['attention_mask'].squeeze(0),
+                "input_ids_prompt": enc_prompt["input_ids"].squeeze(0),
+                "attention_mask_prompt": enc_prompt["attention_mask"].squeeze(0),
             }
 
         elif self.task == "countdown":
-
-            chosen_score = example['chosen_score']
-            rejected_score = example['rejected_score']
-
             return {
-                'input_ids_prompt': enc_prompt['input_ids'].squeeze(0),
-                'attention_mask_prompt': enc_prompt['attention_mask'].squeeze(0),
-                'input_ids_chosen': enc_chosen['input_ids'].squeeze(0),
-                'attention_mask_chosen': enc_chosen['attention_mask'].squeeze(0),
-                'input_ids_rejected': enc_rejected['input_ids'].squeeze(0),
-                'attention_mask_rejected': enc_rejected['attention_mask'].squeeze(0),
-                'chosen_score': chosen_score,
-                'rejected_score': rejected_score,
+                "ground_truth": example["ground_truth"],
+                "input_ids_prompt": enc_prompt["input_ids"].squeeze(0),
+                "attention_mask_prompt": enc_prompt["attention_mask"].squeeze(0),
             }
