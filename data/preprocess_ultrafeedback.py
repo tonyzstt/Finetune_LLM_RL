d01cbf692f2ed2ae4c5a4a3921126dee790d152a
tonyzstt <zhangshutong2000@gmail.com>
2025-05-21 21:12:46 -0700

    update ultrafeedback data script

--- data/preprocess_ultrafeedback.py
+++ data/preprocess_ultrafeedback.py
@@ -13,14 +13,25 @@ def preprocess_ultrafeedback_dataset(dataset, output_file):
     with open(output_file, "w") as f:
         f.write('[')
 
-    num = 100
+    num_samples = 0
+
     for item in tqdm(dataset):
 
-        input = item["prompt"]
+        prompt = item["prompt"]
         chosen = item["chosen"][1]['content']
         rejected = item["rejected"][1]['content']
+
+        prompt_length = len(prompt.split(" "))
+        chosen_length = len(chosen.split(" "))
+        rejected_length = len(rejected.split(" "))
+
+        if prompt_length > 128 or chosen_length > 512 or rejected_length > 512:
+            continue
+
+        num_samples += 1
+
         message = {
-            "input": input,
+            "input": prompt,
             "chosen": chosen,
             "rejected": rejected
         }
@@ -28,10 +39,6 @@ def preprocess_ultrafeedback_dataset(dataset, output_file):
         message = json.dumps(message, indent=4, ensure_ascii=False)
         with open(output_file, "a", encoding='utf-8') as f:
             f.write(message + ",\n")
-
-        num -= 1
-        if num == 0:
-            break
             
     # Remove the last comma and close the JSON array
     with open(output_file, "rb+") as f:
@@ -42,7 +49,7 @@ def preprocess_ultrafeedback_dataset(dataset, output_file):
         f.write(']')
     
     print(f"Preprocessed dataset saved to {output_file}")
-
+    print(f"Number of samples: {num_samples}")
 
 if __name__ == "__main__":
 
@@ -50,5 +57,5 @@ if __name__ == "__main__":
     os.makedirs(output_dir, exist_ok=True)
     ds = load_dataset("HuggingFaceH4/ultrafeedback_binarized", split="train_prefs")
     
-    output_file = os.path.join(output_dir, "ultrafeedback_binarized_train_prefs_small.json")
+    output_file = os.path.join(output_dir, "ultrafeedback_binarized_train_prefs.json")
     preprocess_ultrafeedback_dataset(ds, output_file)
