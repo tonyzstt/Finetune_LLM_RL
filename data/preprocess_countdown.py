4def7196945c02facfe3d98ae5ddd3e45c3712c7
Ubuntu <ubuntu@ip-172-31-22-199.us-west-2.compute.internal>
2025-06-02 17:19:18 +0000

    comment out unneeded part for RLOO

--- data/preprocess_countdown.py
+++ data/preprocess_countdown.py
@@ -14,15 +14,14 @@ def preprocess_countdown_dataset(dataset, output_file, max_length=1024, max_toke
     Preprocess the countdown dataset by adding preferred and dispreferred responses with corresponding scores.
     """
     
-    # TODO: change the model to SFT model
-    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-0.5B")
-    model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-0.5B").cuda()
+    # # TODO: change the model to SFT model
+    # tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-0.5B")
+    # model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-0.5B").cuda()
     
     # Open the JSON array
     with open(output_file, "w") as f:
         f.write("[")
 
-    num = 100
     for item in tqdm(dataset):
 
         numbers = item["nums"]
@@ -35,44 +34,35 @@ def preprocess_countdown_dataset(dataset, output_file, max_length=1024, max_toke
         user = f"<|im_start|>user\nUsing the numbers {numbers}, create an equation that equals {target}. You can use basic arithmetic operations (+, -, *, /) and each number can only be used once. Show your work in <think> </think> tags. And return the final answer in <answer> </answer> tags, for example <answer> (1 + 2) / 3 </answer> <|im_end|>\n"
         assistant = "<|im_start|>assistant\n"
         prompt = user + assistant
-        enc_prompt = tokenizer(prompt, padding='max_length', truncation=True, max_length=max_length, return_tensors="pt")
-        input_ids = enc_prompt['input_ids'].cuda()
-        attention_mask = enc_prompt['attention_mask'].cuda()
-        output_ids1 = model.generate(input_ids=input_ids, attention_mask=attention_mask, max_new_tokens=max_tokens, do_sample=True, temperature=0.7, top_p=0.9, top_k=50, pad_token_id=tokenizer.eos_token_id)
-        output_ids2 = model.generate(input_ids=input_ids, attention_mask=attention_mask, max_new_tokens=max_tokens, do_sample=True, temperature=0.9, top_p=1.0, top_k=30, pad_token_id=tokenizer.eos_token_id)
-        output1 = tokenizer.decode(output_ids1[0][input_ids.shape[1]:], skip_special_tokens=True).strip()
-        output2 = tokenizer.decode(output_ids2[0][input_ids.shape[1]:], skip_special_tokens=True).strip()
-        # print(f"Prompt: {prompt}\n\n")
-        # print(f"Output 1: {output1}\n\n")
-        # print(f"Output 2: {output2}\n\n")
-        score1 = countdown.compute_score(output1, ground_truth)
-        score2 = countdown.compute_score(output2, ground_truth)
-        # print(f"Score 1: {score1}")
-        # print(f"Score 2: {score2}")
-        if score1 > score2:
-            chosen = output1
-            rejected = output2
-        elif score1 < score2:
-            chosen = output2
-            rejected = output1
-        # filtering out for ties
-        else:
-            continue
+        # enc_prompt = tokenizer(prompt, padding='max_length', truncation=True, max_length=max_length, return_tensors="pt")
+        # input_ids = enc_prompt['input_ids'].cuda()
+        # attention_mask = enc_prompt['attention_mask'].cuda()
+        # output_ids1 = model.generate(input_ids=input_ids, attention_mask=attention_mask, max_new_tokens=max_tokens, do_sample=True, temperature=0.7, top_p=0.9, top_k=50, pad_token_id=tokenizer.eos_token_id)
+        # output_ids2 = model.generate(input_ids=input_ids, attention_mask=attention_mask, max_new_tokens=max_tokens, do_sample=True, temperature=0.9, top_p=1.0, top_k=30, pad_token_id=tokenizer.eos_token_id)
+        # output1 = tokenizer.decode(output_ids1[0][input_ids.shape[1]:], skip_special_tokens=True).strip()
+        # output2 = tokenizer.decode(output_ids2[0][input_ids.shape[1]:], skip_special_tokens=True).strip()
+        # score1 = countdown.compute_score(output1, ground_truth)
+        # score2 = countdown.compute_score(output2, ground_truth)
+        # if score1 > score2:
+        #     chosen = output1
+        #     rejected = output2
+        # elif score1 < score2:
+        #     chosen = output2
+        #     rejected = output1
+        # # filtering out for ties
+        # else:
+        #     continue
 
         message = {
             "input": prompt,
-            "chosen": chosen,
-            "rejected": rejected,
+            # "chosen": chosen,
+            # "rejected": rejected,
             "ground_truth": ground_truth,
         }
         
         message = json.dumps(message, indent=4, ensure_ascii=False)
         with open(output_file, "a", encoding="utf-8") as f:
             f.write(message + ",\n")
-
-        num -= 1
-        if num == 0: 
-            break
             
     # Remove the last comma and close the JSON array
     with open(output_file, "rb+") as f:
@@ -90,5 +80,5 @@ if __name__ == "__main__":
     output_dir = "../processed_dataset"
     os.makedirs(output_dir, exist_ok=True)
     ds = load_dataset("Jiayi-Pan/Countdown-Tasks-3to4", split="train")
-    output_file = os.path.join(output_dir, "countdown_train_small.json")
+    output_file = os.path.join(output_dir, "countdown_train.json")
     preprocess_countdown_dataset(ds, output_file)
